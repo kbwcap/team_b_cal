65df8ec745fd20cea85fb2141030da6c0492a033
yuz010 <rladbwmd123@gmail.com>
2025-03-26 17:51:47 +0900

    add - getZegop, getMinus

--- Project7/main.cpp
+++ Project7/main.cpp
@@ -3,6 +3,9 @@
 class Cal {
  public:
   int GetSum(int a, int b) { return a + b; }
+  int getZegop(int a) { return a * a; }
+  int getMinus(int a, int b) { return a - b; }
+
   int getDivide(int a, int b) {
     if (b == 0) return 0;
     return a / b;
@@ -16,6 +19,18 @@ TEST(TEAMB, test_add) {
   EXPECT_EQ(ret, 2);
 }
 
+TEST(TEAMB, test_zegop) {
+    Cal cal;
+    int ret = cal.getZegop(3);
+    EXPECT_EQ(ret, 9);
+}
+
+TEST(TEAMB, test_minus) {
+    Cal cal;
+    int ret = cal.GetSumSum(3, 4);
+    EXPECT_EQ(ret, -1);
+}
+
 TEST(TEAMB, test_devide) {
   Cal cal;
   int ret = cal.getDivide(10, 5);
@@ -27,6 +42,7 @@ TEST(TEAMB, test_addadd) {
   int ret = cal.GetSumSum(1, 1, 1);
   EXPECT_EQ(ret, 3);
 }
+
 int main() {
   ::testing::InitGoogleMock();
   return RUN_ALL_TESTS();
